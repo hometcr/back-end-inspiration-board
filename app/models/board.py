9ba449553fa49cb5edc623838465a1ce8fd7a1db
Caitlyn <caitlyn.homet@gmail.com>
2023-01-03 13:43:15 -0500

    back end set up

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,7 @@
 from app import db
+
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    owners_name = db.Column(db.String)
+    # cards = db.relationship("Card", back_populates="board")
--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,9 @@
 from app import db
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    likes_count = db.Column(db.Integer)
+    message = db.Column(db.String)
+    # board_id = db.Column(db.Integer, db.ForeignKey(
+        # 'board.board_id'), nullable=True)
+    # board = db.relationship("Board", back_populates="cards", lazy=True)
